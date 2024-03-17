00c8f51d1f18e4b62ac39f6905fcf54cec10c5b0
Nida Rizwan <143074620+NidzR@users.noreply.github.com>
2024-03-18 00:53:41 +0500

    Software Installation_keys
    Software Installation_keys

---
+++ index.ts
@@ -0,0 +1,4 @@
+// 1. Install [Node.js](https://nodejs.org/en/), [TypeScript](https://www.typescriptlang.org/download)  and [VS Code](https://code.visualstudio.com/) on your computer.
+
+// npm i inquirer
+// tsc --init
